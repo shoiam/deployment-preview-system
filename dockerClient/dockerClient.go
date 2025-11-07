eee36d56a3d88be328c6c2c386c211e60010f957
shoiam <shoaibfasmd@proton.me>
2025-11-07 12:34:17 +0530

    Add Image listing docker client

--- dockerClient/dockerClient.go
+++ dockerClient/dockerClient.go
@@ -14,13 +14,22 @@ func ClientElement() {
 	}
 	defer apiClient.Close()
 
-	containers, err := apiClient.ContainerList(context.Background(), client.ContainerListOptions{})
+	containers, containerErr := apiClient.ContainerList(context.Background(), client.ContainerListOptions{})
+	images, imageErr := apiClient.ImageList(context.Background(), client.ImageListOptions{All: true})
 
-	if err != nil {
-		panic(err)
+	if containerErr != nil {
+		panic(containerErr)
+	}
+
+	if imageErr != nil {
+		panic(imageErr)
 	}
 
 	for _, ctr := range containers.Items {
 		fmt.Printf("%s %s (status: %s)\n", ctr.ID, ctr.Image, ctr.Status)
+	}
+
+	for _, image := range images.Items {
+		fmt.Printf("%s %s (status: %s)\n", image.ID, image.ParentID, image.Labels[image.Descriptor.Digest.Algorithm().String()])
 	}
 }
